62c57ba7fa072f0a9af89aa2913876c4f9cf74c6
Vitor <vitor.manoel@ajdesenvolvimento.com.br>
2024-12-26 22:17:01 -0300

    first commit

---
+++ 1 - 217. Contains Duplicate/ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> hashset = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (hashset.Contains(num))
+            {
+                return true;
+            } 
+            else
+            {
+                hashset.Add(num);
+            }
+        }
+
+        return false;
+    }
+}
